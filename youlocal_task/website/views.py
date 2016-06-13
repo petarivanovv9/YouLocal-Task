79aec48b39f722280d67e65fe6d2a3496aad88ef
Petar Ivanov <pepincho95@gmail.com>
2016-06-13 21:58:23 +0300

    Add simple venues listing.

--- youlocal_task/website/views.py
+++ youlocal_task/website/views.py
@@ -8,11 +8,33 @@ import requests
 
 
 def index(request):
-    #raw_venues = __get_venues()
+    venues = __get_venues()
     return render(request, 'index.html', locals())
 
 
 def __get_venues():
     api_request = URL + LL + RADIOUS + INTENT + CLIENT_ID + CLIENT_SECRET + V + LIMIT
     info = requests.get(api_request).json()
-    return info
+
+    venues = info['response']['venues']
+    formated_venus = []
+
+    for item in venues:
+        try:
+            name = item['name']
+            address = item['location']['address']
+            # primary_category = None
+            distance = item['location']['distance']
+        except:
+            print("There no such fields!")
+
+        print(name + ' -- ' + address + ' -- ' + str(distance))
+
+        venue = {}
+        venue['name'] = name.encode('utf-8')
+        venue['address'] = address
+        venue['distance'] = str(distance)
+
+        formated_venus.append(venue)
+
+    return formated_venus
