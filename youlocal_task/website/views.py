eeead064d132d69691a6b5223338a46ca5b7586f
Petar Ivanov <pepincho95@gmail.com>
2016-06-14 22:00:21 +0300

    Add get_venue_by_id function.

--- youlocal_task/website/views.py
+++ youlocal_task/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, HttpResponse
+from django.shortcuts import render, redirect
 
 from local_settings import *
 import requests
@@ -8,7 +8,7 @@ import requests
 
 
 def index(request):
-    venues = __get_venues()
+    venues = __get_venues_in_radius()
     return render(request, 'index.html', locals())
 
 
@@ -24,25 +24,22 @@ def save_venues(request):
         print i
         print j
         print 30 * '-'
+        print __get_venue_by_id(j)
+        print __get_venue_by_id(j)['name']
     # print(request.POST.lists()[-1][-1][])
     # print(request.POST.lists()[-1][-1])
 
-    print('blqblqblq')
-    # print(request.POST.dict())
-
-    #print(dir(request.POST['venues[]']))
-    # print(request.POST.get('venues[]'))
-    # print(request.POST.get('venues[]'))
     print('qsha')
 
     return redirect('index')
 
-def __get_venues():
-    api_request = URL + LL + RADIOUS + INTENT + CLIENT_ID + CLIENT_SECRET + V + LIMIT
+def __get_venues_in_radius():
+    api_request = URL + 'search' + '?' + LL + RADIOUS + INTENT + '&' + CLIENT_ID + '&' + CLIENT_SECRET + V + LIMIT
     info = requests.get(api_request).json()
-
+    if requests.get(api_request).status_code != 200:
+        return None
     venues = info['response']['venues']
-    formated_venus = []
+    formated_venues = []
 
     for item in venues:
         try:
@@ -62,6 +59,36 @@ def __get_venues():
         venue['address'] = address
         venue['distance'] = distance
 
-        formated_venus.append(venue)
+        formated_venues.append(venue)
 
-    return formated_venus
+    return formated_venues
+
+
+def __get_venue_by_id(id_venue):
+    api_request = URL + id_venue + '?' + CLIENT_ID + '&' + CLIENT_SECRET + V + '&' + LL
+
+    print api_request
+
+    info = requests.get(api_request).json()
+    if requests.get(api_request).status_code != 200:
+        return None
+    raw_venue = info['response']['venue']
+
+    try:
+        id = raw_venue['id']
+        name = raw_venue['name']
+        address = raw_venue['location']['address']
+        # primary_category = None
+        distance = raw_venue['location']['distance']
+    except:
+        print("There no such fields!")
+
+    print(id + ' -- ' + name + ' -- ' + address + ' -- ' + str(distance))
+
+    venue = {}
+    venue['id'] = id
+    venue['name'] = name
+    venue['address'] = address
+    venue['distance'] = distance
+
+    return venue
