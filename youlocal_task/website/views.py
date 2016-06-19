8b0ce8b37cca23fb132e4d33f7411492d1f32d39
Petar Ivanov <pepincho95@gmail.com>
2016-06-19 17:28:38 +0300

    Add single record inserting and updating in mongodb.

--- youlocal_task/website/views.py
+++ youlocal_task/website/views.py
@@ -2,26 +2,51 @@ from django.shortcuts import render, redirect
 
 from local_settings import *
 import requests
+import datetime
 
+from pymongo import MongoClient
 
-# Create your views here.
+client = MongoClient()
+db = client.mydb
 
 
 def index(request):
     venues = __get_venues_in_radius()
+    print db.collection_names(include_system_collections=False)
+    print db.venues.count()
     return render(request, 'index.html', locals())
 
 
 def save_venues(request):
-    #print(request.POST.lists())
+
+    venues = db.venues
+
+
+    print(request.POST.lists())
     print(request.POST.lists()[-1][-1])
     #print('da mu ebesh')
-    for i, j in enumerate(request.POST.lists()[-1][-1]):
-        try:
-            print __get_venue_by_id(j)
-            #print __get_venue_by_id(j)['name']
-        except:
-            continue
+
+    venue = __get_venue_by_id(request.POST.lists()[-1][-1][0])
+    venue['_id'] = venue['id']
+    patka = {'_id': venue['id']}
+    del venue['id']
+    venue['date'] = datetime.datetime.utcnow()
+    print venue
+    venue_id = venues.update(patka, venue, upsert=True)
+    print venue_id
+
+    # for i, j in enumerate(request.POST.lists()[-1][-1]):
+    #     try:
+    #         print 'qshaaa'
+    #         venue = __get_venue_by_id(j)
+    #         venue['date'] = datetime.datetime.utcnow()
+    #         print venue
+    #         venue_id = venues.insert_one(venue).inserted_id
+    #         print venue_id
+    #         #print __get_venue_by_id(j)['name']
+    #     except:
+    #         continue
+
 
     return redirect('index')
 
